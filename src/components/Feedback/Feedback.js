56f7604fa0b9d62a8ae27e1d0c397785b5692de7
camariu <amariutei1998@gmail.com>
2023-08-25 14:00:05 +0300

    hw-02

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from './Feedback.module.css';
 
 export default function Feedback({handleGood, handleNeutral, handleBad}) {
